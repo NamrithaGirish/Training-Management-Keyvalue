e057dea6845d57cf7101db41a5fbc49da8a8033c
anoop <anooppkr2003@gmail.com>
2025-06-16 10:51:32 +0530

    training-updated-and-migration-added

---
+++ backend/entities/training.enity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, OneToMany } from "typeorm";
+import AbstractBaseEntity from "./abstract.entity";
+import { Session } from "./session.entity";
+
+@Entity()
+export class Training extends AbstractBaseEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ type: "date" })
+  startDate: Date;
+
+  @Column({ type: "date" })
+  endDate: Date;
+
+  @OneToMany(() => Session, (session) => session.Training)
+  sessions: Session[];
+}
