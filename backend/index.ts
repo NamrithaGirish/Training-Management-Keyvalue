990ad8ca668773ae92c67eeddcf4720e9d548f13
Aadithya Sai <aadithyas0610@gmail.com>
2025-06-16 10:45:04 +0530

    added auth middleware to index

--- backend/index.ts
+++ backend/index.ts
@@ -6,6 +6,7 @@ import LoggerService from "./services/logger.service";
 import userRouter from "./routes/user.route";
 import authRouter from "./routes/auth.routes";
 import errorMiddleware from "./middlewares/errorMiddleware";
+import authMiddleware from "./middlewares/auth.middleware";
 
 const PORT = 3000;
 
@@ -18,23 +19,26 @@ server.use(express.json());
 
 // Global routes
 server.get("/health", (req, res) => {
-	res.status(200).json({ message: "Server is healthy" });
+  res.status(200).json({ message: "Server is healthy" });
 });
 
 // Import routes
 server.use("/auth", authRouter);
+
+// server.use(authMiddleware); // disabled while dev
+
 server.use("/users", userRouter);
 server.use(errorMiddleware);
 
 (async () => {
-	try {
-		await dataSource.initialize();
-		logger.info("Connected to db");
-		server.listen(PORT, () => {
-			logger.info(`Server listening to ${PORT}`);
-		});
-	} catch (error) {
-		logger.error(`Failed to connect to db - ${error.message}`);
-		process.exit(0);
-	}
+  try {
+    await dataSource.initialize();
+    logger.info("Connected to db");
+    server.listen(PORT, () => {
+      logger.info(`Server listening to ${PORT}`);
+    });
+  } catch (error) {
+    logger.error(`Failed to connect to db - ${error.message}`);
+    process.exit(0);
+  }
 })();
