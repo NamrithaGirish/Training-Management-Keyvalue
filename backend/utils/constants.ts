65b3da03ea4d5fdccbb9d8912060e61ae3a80dff
NamrithaGirish <namrithac1723@gmail.com>
2025-06-17 11:49:45 +0530

    Production related changes

--- backend/utils/constants.ts
+++ backend/utils/constants.ts
@@ -1,9 +1,7 @@
-import { StringValue } from "ms";
-
 export const JWT_SECRET: string = process.env.JWT_SECRET;
-export const JWT_VALIDITY: StringValue = process.env
-	.JWT_VALIDITY as StringValue;
+export const JWT_VALIDITY: string = process.env.JWT_VALIDITY;
 
+export const NODE_ENV = process.env.NODE_ENV;
 export const FASTAPI_URL: string = process.env.FASTAPI_ENDPOINT;
 
 export const ENABLE_AI: boolean = process.env.ENABLE_AI === "true";
