805c9cb2d9ca3d6c62b9adfb8158f84657186b20
Aadithya Sai <aadithyas0610@gmail.com>
2025-06-17 10:15:12 +0530

    made authService sttaic

--- backend/middlewares/authorization.middleware.ts
+++ backend/middlewares/authorization.middleware.ts
@@ -4,34 +4,34 @@ import HTTPException from "../exceptions/http.exception";
 import AuthorizationService from "../services/authorization.service";
 import { userService } from "../routes/user.route";
 
-export default function validRoles(allowedRoles: string[]) {
-  const authorizationService = new AuthorizationService(userService);
+const authorizationService = new AuthorizationService(userService);
 
-  async function checkAccess(
-    allowedRoles: string[],
-    userId: number,
-    sessionId?: number
-  ) {
-    if (allowedRoles.includes("ALL")) {
-      return true;
-    } else if (allowedRoles.includes("ADMIN")) {
-      return await authorizationService.isAdmin(userId);
-    } else if (allowedRoles.includes("MODERATOR")) {
-      return (
-        sessionId && (await authorizationService.isModerator(userId, sessionId))
-      );
-    } else if (allowedRoles.includes("TRAINER")) {
-      return (
-        sessionId && (await authorizationService.isTrainer(userId, sessionId))
-      );
-    } else if (allowedRoles.includes("CANDIDATE")) {
-      return (
-        sessionId && (await authorizationService.isCandidate(userId, sessionId))
-      );
-    }
-    return false;
+async function checkAccess(
+  allowedRoles: string[],
+  userId: number,
+  sessionId?: number
+) {
+  if (allowedRoles.includes("ALL")) {
+    return true;
+  } else if (allowedRoles.includes("ADMIN")) {
+    return await authorizationService.isAdmin(userId);
+  } else if (allowedRoles.includes("MODERATOR")) {
+    return (
+      sessionId && (await authorizationService.isModerator(userId, sessionId))
+    );
+  } else if (allowedRoles.includes("TRAINER")) {
+    return (
+      sessionId && (await authorizationService.isTrainer(userId, sessionId))
+    );
+  } else if (allowedRoles.includes("CANDIDATE")) {
+    return (
+      sessionId && (await authorizationService.isCandidate(userId, sessionId))
+    );
   }
+  return false;
+}
 
+export default function validRoles(allowedRoles: string[]) {
   return async function (req: Request, res: Response, next: NextFunction) {
     try {
       if (!req.user) {
