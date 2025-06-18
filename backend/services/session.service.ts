22adfc66affc434f98ef82bcd266d911c4fa062d
sreepriya <sreepriya.mec@gmail.com>
2025-06-18 12:50:54 +0530

    updated user-session-with-role-check

--- backend/services/session.service.ts
+++ backend/services/session.service.ts
@@ -5,10 +5,8 @@ import { CreateSessionDto, UpdateSessionDto } from "../dto/session.dto";
 import LoggerService from "./logger.service";
 import HTTPException from "../exceptions/http.exception";
 import { UserSessionRepository } from "../repositories/user-session.repository";
-
 import { CreateUserSessionDto, DeleteUserSessionDto } from "../dto/user-session.dto";
 import {  UserSession } from "../entities/user-session.entity";
-
 import UserRepository from "../repositories/user.repository";
 import UserService from "./user.service";
 import TrainingService from "./training.service";
@@ -16,6 +14,7 @@ import { error } from "console";
 
 import { Role } from "../entities/training-users.entity";
 import { userService } from "../routes/user.route";
+
 
 export class SessionService {
 	private logger = LoggerService.getInstance("UserService()");
